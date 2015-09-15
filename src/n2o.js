84c1cf8999135c779470decdc39ad53b31d47a08
Namdak Tonpa <maxim@synrc.com>
2015-09-15 04:13:56 +0300

    init

---
+++ src/n2o.js
@@ -0,0 +1,27 @@
+var net = require('net');
+    util = require('util');
+
+var clients = [];
+
+net.createServer(function (socket) {
+    socket.name = socket.remoteAddress + ":" + socket.remotePort;
+    console.log('connection');
+    clients.push(socket);
+    socket.write("{io," + socket.name + ",[]}");
+    broadcast(socket.name + " joined the chat\n", socket);
+    socket.on('data', function (data) {
+        console.log(data);
+        broadcast(socket.name + "> " + data, socket);
+    });
+    socket.on('end', function () {
+        clients.splice(clients.indexOf(socket), 1);
+        broadcast(socket.name + " left the chat.\n");
+    });
+    function broadcast(message, sender) {
+        clients.forEach(function (client) {
+        if (client === sender) return;
+        client.write(message);
+    });
+    process.stdout.write(message)
+    }
+}).listen(5000);
